c90a8486f973fbfb224d300c922d9f0c1f402975
DomenicoMereu97 <75221419+DomenicoMereu97@users.noreply.github.com>
2023-02-17 16:56:47 +0100

    Add files via upload

---
+++ Thext/__init__.py
@@ -0,0 +1,4 @@
+from Thext.DatasetPlus import DatasetPlus
+from Thext.Highlighter import Highlighter
+from Thext.RedundancyManager import RedundancyManager
+from Thext.SentenceRankerPlus import SentenceRankerPlus
